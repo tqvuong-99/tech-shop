9d84d1d74de2c1c61fe31547492b530f3d5f97d7
tqvuong-99 <tranquocvuong2102elec@gmail.com>
2025-03-26 13:26:06 +0700

    first command

---
+++ frontend/app/page.tsx
@@ -0,0 +1,12 @@
+export default function Home() {
+
+  console.log("Home page");
+
+  return (
+    <main>
+      <div className="container mx-auto">
+      <h1 className="font-bold text-2xl">Hello world</h1>
+      </div>
+    </main>
+  );
+}
